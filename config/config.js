0de48c77d1e6fe1e08867f83c1f9677731196a99
Ryan Kurte <ryankurte@gmail.com>
2015-12-18 22:43:59 +1300

    Working on heroku

--- config/config.js
+++ config/config.js
@@ -11,11 +11,11 @@ config = {
   },
   "test": {
     url: "localhost",
-    "database": "sqlite://test.db",
+    database: "sqlite://test.db",
   },
   "production": {
     url: process.env.URL,
-    "database": process.env.DATABASE_URL
+    database: process.env.DATABASE_URL
   }
 }
 
