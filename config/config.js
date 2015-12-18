6495730567fed56133c011afb32882c48e142472
Ryan Kurte <ryankurte@gmail.com>
2015-12-18 22:24:19 +1300

    Kinda seems like a good idea now.

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ var env = process.env.NODE_ENV || "development";
 
 config = {
   "development": {
+    url: "localhost",
     database: "sqlite://dev.db",
     options: {
         logging: false,
@@ -9,9 +10,11 @@ config = {
     },
   },
   "test": {
+    url: "localhost",
     "database": "sqlite://test.db",
   },
   "production": {
+    url: process.env.URL,
     "database": process.env.DATABASE_URL
   }
 }
