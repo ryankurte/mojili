1900ffe58cf4dfc58fb84c72ecf327b1712d1008
Jeff <peabnuts123@gmail.com>
2015-12-21 13:03:51 +1300

    FIxed front end to be a real thing. Used things properly. Moved JavaScript into JS files. Moved CSS into CSS files. Added new method to Backend to fetch random emoji for Title on page, for the lulz.

---
+++ static/js/mojili.js
@@ -0,0 +1,52 @@
+(function mojili() {
+    $(function onReady() {
+        setTimeout(function getAndSetRandomEmoji() {
+            var randomEmoji = $.get('/emoji', function (data) {
+                if (data.result === 'okay') {
+                    $("#titleHeader").prepend(data.emoji + " ");
+                }
+            });
+        });
+    });
+
+
+    function getURL() {
+        return $('#url').val().toLowerCase().trim();
+    }
+
+    submitURL = function submitURL() {
+        var url = getURL();
+
+        $('#result').toggleClass('hidden', true);
+
+        $.post('/', {url: url}, function (data) {
+            $('#result').toggleClass('hidden', false);
+
+            if (data.result == 'okay') {
+                $('#result').empty();
+                $('#result').toggleClass('alert-success', true);
+                $('#result').toggleClass('alert-danger', false);
+
+                $('#result').append('<p><strong>Your Link:</strong> <a href="/' + data.emoji_url + '">moji.li/' + data.emoji_url + '</a></p>');
+            } else {
+                $('#result').empty();
+                $('#result').toggleClass('alert-success', false);
+                $('#result').toggleClass('alert-danger', true);
+                $('#result').append('<p>' + data.message + '</p>');
+            }
+        });
+    }
+
+    onURLChange = function onURLChange() {
+        var url = getURL();
+
+        var error = false;
+
+        if (url.indexOf('http://') == -1 && url.indexOf('https://')) {
+            error = true;
+        }
+
+        $('#submitButton').prop('disabled', error);
+        $('#errorMessage').toggleClass('hidden', !error);
+    }
+})();
